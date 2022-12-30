910e5a0fd58a71dfced9176d9913ca0468047540
nubdevs <idkbharti@gmail.com>
2022-12-30 10:50:34 -0800

    dynamic routing added to collegePage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import ForumPage from "./pages/forumPage";
 import HomePage from "./pages/homePage";
 import NewsPage from "./pages/newsPage";
 import ToolsPage from "./pages/toolsPage";
-// import {Footer} from "./components/Footer/footer"  
 import CollegeFooter from "./components/Footer/CollegeFooter";
 import ScrollToTop from "./ScrollToTop"
 import MainNavbar from "./components/Navbar/MainNavbar";
@@ -21,7 +20,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage/>} />
       <Route path="/admission" element={<AdmissionPage/>} />
-      <Route path="/particularcollege" element={<CollegePage/>} />
+      <Route path="/:particularcollege" element={<CollegePage/>} />
       <Route path="/news" element={<NewsPage/>} />
       <Route path="/exams" element={<ExamPage/>} />
       <Route path="/forum" element={<ForumPage/>} />
