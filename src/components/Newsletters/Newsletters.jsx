cd7ed942906529dffaf2a231fda52d831ee4915c
Prince Choudhary <princechoudhary784@gmail.com>
2022-12-30 15:41:03 +0530

    college prediction & Newsletters

---
+++ src/components/Newsletters/Newsletters.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+function Newsletters() {
+  return (
+    <div className='mb-6'>
+      <h2 className='my-6 text-[20px] font-semibold text-center text-[#303030]'>Newsletters</h2>
+      <div className="box m-auto mx-3 flex px-5 py-2 mb-6" style={{
+        boxShadow: "1px 2px 6px 1px rgba(0, 0, 0, 0.13)",
+        borderRadius: "5px"
+      }}>
+        <img src="src\components\Newsletters\news.png" alt="" className='w-10 h-10'/>
+        <div className="content flex-col ml-3">
+          <h3 className='text-[14px] font-semibold leading-4 mb-1'>JEE 2022 Result Declare, Download Your JEE 2022 Score Card</h3>
+          <h4 className='text-[#646464] text-[12px] leading-4 mb-2'>All students are informed that JEE results has been announced by NTA 2022 for all students, Check complete details Here </h4>
+          <div className="bottom flex justify-between">
+            <p className="date text-[#747474] text-[12px]">Dec 19, 2022</p>
+            <div className="more flex w-[75px] justify-between">
+              <p className='text-[12px] font-semibold'>Read more</p>
+              <img src="src\components\Newsletters\right.png" alt="" className='h-[10px] w-[10px] mt-[5px]'/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="box m-auto mx-3 flex px-5 py-2 mb-6" style={{
+        boxShadow: "1px 2px 6px 1px rgba(0, 0, 0, 0.13)",
+        borderRadius: "5px"
+      }}>
+        <img src="src\components\Newsletters\news.png" alt="" className='w-10 h-10'/>
+        <div className="content flex-col ml-3">
+          <h3 className='text-[14px] font-semibold leading-4 mb-1'>JEE 2022 Result Declare, Download Your JEE 2022 Score Card</h3>
+          <h4 className='text-[#646464] text-[12px] leading-4 mb-2'>All students are informed that JEE results has been announced by NTA 2022 for all students, Check complete details Here </h4>
+          <div className="bottom flex justify-between">
+            <p className="date text-[#747474] text-[12px]">Dec 19, 2022</p>
+            <div className="more flex w-[75px] justify-between">
+              <p className='text-[12px] font-semibold'>Read more</p>
+              <img src="src\components\Newsletters\right.png" alt="" className='h-[10px] w-[10px] mt-[5px]'/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="box m-auto mx-3 flex px-5 py-2 mb-6" style={{
+        boxShadow: "1px 2px 6px 1px rgba(0, 0, 0, 0.13)",
+        borderRadius: "5px"
+      }}>
+        <img src="src\components\Newsletters\news.png" alt="" className='w-10 h-10'/>
+        <div className="content flex-col ml-3">
+          <h3 className='text-[14px] font-semibold leading-4 mb-1'>JEE 2022 Result Declare, Download Your JEE 2022 Score Card</h3>
+          <h4 className='text-[#646464] text-[12px] leading-4 mb-2'>All students are informed that JEE results has been announced by NTA 2022 for all students, Check complete details Here </h4>
+          <div className="bottom flex justify-between">
+            <p className="date text-[#747474] text-[12px]">Dec 19, 2022</p>
+            <div className="more flex w-[75px] justify-between">
+              <p className='text-[12px] font-semibold'>Read more</p>
+              <img src="src\components\Newsletters\right.png" alt="" className='h-[10px] w-[10px] mt-[5px]'/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="box m-auto mx-3 flex px-5 py-2 mb-6" style={{
+        boxShadow: "1px 2px 6px 1px rgba(0, 0, 0, 0.13)",
+        borderRadius: "5px"
+      }}>
+        <img src="src\components\Newsletters\news.png" alt="" className='w-10 h-10'/>
+        <div className="content flex-col ml-3">
+          <h3 className='text-[14px] font-semibold leading-4 mb-1'>JEE 2022 Result Declare, Download Your JEE 2022 Score Card</h3>
+          <h4 className='text-[#646464] text-[12px] leading-4 mb-2'>All students are informed that JEE results has been announced by NTA 2022 for all students, Check complete details Here </h4>
+          <div className="bottom flex justify-between">
+            <p className="date text-[#747474] text-[12px]">Dec 19, 2022</p>
+            <div className="more flex w-[75px] justify-between">
+              <p className='text-[12px] font-semibold'>Read more</p>
+              <img src="src\components\Newsletters\right.png" alt="" className='h-[10px] w-[10px] mt-[5px]'/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Newsletters
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import {TopColleges} from "../components/TopColleges/topColleges"
-import TakeInformedDecisions from "../components/TakeInformedDecisions/takeInformedDecision"
-import KeepUpdated from "../components/KeepYourselfUpdated/keepUpdated"
-import CollegePredictor from '../components/collegePredictor/collegePredictor'
+import CollegesPredictor from '../components/CollegesPredictor/CollegesPredictor'
+import Newsletters from '../components/Newsletters/Newsletters'
 
 const HomePage = () => {
   return (
     <>
-    {/* main header to be added */}
-    <CollegePredictor/>
-    <TakeInformedDecisions></TakeInformedDecisions>
-    <KeepUpdated></KeepUpdated>
-    <TopColleges/>
+      <CollegesPredictor />
+      <Newsletters/>
     </>
   )
 }
