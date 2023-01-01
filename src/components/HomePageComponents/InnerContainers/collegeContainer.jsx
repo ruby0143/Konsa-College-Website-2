680b2c17bea3a824ef0244fd945391c314b227f5
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-01-01 21:47:15 +0530

    [collegePageHeader.jsx] collegePageHeader Banner fixed

--- src/components/HomePageComponents/InnerContainers/collegeContainer.jsx
+++ src/components/HomePageComponents/InnerContainers/collegeContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CollegeContainer = ({collegeName, collegeLogo, collegeBanner}) => {
   return (
     <div className='min-w-[210px] h-[164px] shadow-md rounded-md border border-gray-100 bg-white'>
-        <div className='.backgroundContainer relative w-full h-[108px] rounded-t-md flex items-end justify-center' >
+        <div className='relative w-full h-[108px] rounded-t-md flex items-end justify-center' >
             <div className={`absolute w-full h-[108px] brightness-50`} >
               <img src={collegeBanner} alt="College Banner" className='w-[210px] h-[108px]'/>
             </div>
