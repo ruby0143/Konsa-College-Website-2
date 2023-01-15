48f930aa775db7cbb23cdea017fd7a23add16b7d
devkumar <idkbharti@gmail.com>
2023-01-15 11:41:57 +0530

    allclgs routes created

---
+++ src/components/AllColleges/AllColleges.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AllClgSearchBar from './Components/AllClgSearchBar'
+
+const AllColleges = () => {
+  return (
+    <AllClgSearchBar/>
+  )
+}
+
+export default AllColleges
