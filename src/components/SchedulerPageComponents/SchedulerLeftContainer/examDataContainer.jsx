535b75ae319551e2bf1fd0a907abd58ff5a79881
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-01-31 15:08:09 +0530

    scheduler page, mobile view updated, image added

--- src/components/SchedulerPageComponents/SchedulerLeftContainer/examDataContainer.jsx
+++ src/components/SchedulerPageComponents/SchedulerLeftContainer/examDataContainer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const ExamDataContainer = ({exam}) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' }
   return (
     <div className='bg-white rounded-md shadow-md h-[186px] w-[48%] mb-4'>
       <div className='h-[50%]'>
         <img 
             src={exam.img} 
             alt="exam img"
-            className='h-full bg-slate-400 rounded-t-md'
+            className='h-full w-full object-fill bg-slate-400 rounded-t-md'
         />
       </div>
       <div className='h-[50%] p-3 flex flex-col justify-between'>
@@ -15,9 +16,9 @@ const ExamDataContainer = ({exam}) => {
             <div className='font-light'>
                 {exam.exam_name}
             </div>
-            <div className='text-[10px] font-medium justify-self-end'>
-                Last date to apply: 22 feb 2023
-            </div>
+            <div className='text-[9px] font-medium justify-self-end'>
+                Last date to apply: {new Intl.DateTimeFormat('en-US', options).format(new Date(exam.date))}
+            </div>  
         </div>
         <button className='w-full rounded-full bg-white border border-[#EE7C00] text-[10px] py-[2px] font-medium text-[#EE7C00]'>
             Apply Now
