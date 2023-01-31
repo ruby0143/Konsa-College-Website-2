0a921493756a2dead99da6ad1e037ffd54a41969
ruby0143 <mysoresriharsha07@gmail.com>
2023-01-31 22:32:16 +0530

    private route

--- src/pages/collegePage.jsx
+++ src/pages/collegePage.jsx
@@ -11,28 +11,32 @@ import Placements from "../components/ParticularCollege/Placements";
 import CollegePageHeader from "../components/Header/collegePageHeader/collegePageHeader";
 import axios from "axios";
 import RightSection from "../components/ParticularCollege/RightSection";
+import { FaPassport } from "react-icons/fa";
 
 const CollegePage = () => {
-
-  const [result, setResult] = useState([])
-
-  const path = useLocation().pathname
+  const [result, setResult] = useState([]);
+  const [admin, setAdmin] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const path = useLocation().pathname;
 
   useEffect(() => {
-    axios.get(`https://konsa-college-backend-production.up.railway.app${path}`)
-      .then(res => {
-        console.log(res.data)
-        setResult(res.data)
+    const pass = searchParams.get("password");
+    if (pass === "FamLearn123") {
+      setAdmin(true);
+    }
+    axios
+      .get(`http://localhost:5000${path}`)
+      .then((res) => {
+        console.log(res.data);
+        setResult(res.data);
       })
-      .catch(err => console.log("error: ", err))
-  }, [])
+      .catch((err) => console.log("error: ", err));
+  }, []);
 
   return (
     <>
       {result ? (
-        <>
-        {/* left  */}
-        <div className="bg-[#F5F5F5] md:max-w-[70%]">
+        <div className="bg-[#F5F5F5]">
           <CollegePageHeader result={result} />
           <Overview result={result}></Overview>
           <Connectivity result={result} />
@@ -42,16 +46,15 @@ const CollegePage = () => {
           <Cutoff result={result} />
           <Placements result={result} />
           <CollegeReview result={result} />
+          {admin ? (
+            <a href="https://google.com" className="grid  w-12" style={{margin:"auto"}}>
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Approve</button>
+            </a>
+          ) : null}
         </div>
-        {/* right  */}
-        <div className="bg-[#F5F5F5] md:max-w-[30%] hidden md:visible">
-          <RightSection result={result}></RightSection>
-        </div>
-        </>
-  ): (<div>
-    No College Found
-  </div>)
-}
+      ) : (
+        <div>No College Found</div>
+      )}
     </>
   );
 };
