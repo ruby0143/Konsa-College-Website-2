bf2be7b51a12fbd49ed619f1901bfba377b52dc7
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-02-02 21:32:07 +0530

    [toolsHead.jsx] ui updated

--- src/components/toolsPage/toolsHead.jsx
+++ src/components/toolsPage/toolsHead.jsx
@@ -18,21 +18,17 @@ function toolsHead(props) {
             className="h-full w-full"
           />
         </div>
-        <div className="absolute ">
+        <div className="absolute w-full md:flex md:items-center md:flex-col">
           <div className="mt-14">
             <div className="text-white m-6 text-xl leading-6 mb-[.8rem] md:text-center">
-              <div className="m-2 text-left text-white">Percentile Predictor</div>
-
-              <div className="text-left text-white m-2 mt-6">JEE Mains 2023</div>
+              <div className="m-2 text-left text-white md:text-center md:text-2xl">Percentile Predictor</div>
+              <div className="text-left text-white m-2 mt-6 md:text-center md:text-2xl">JEE Mains 2023</div>
             </div>
           </div>
 
-          <div className="m-7 mt-16 sm:mt-20">
-            <p className="text-white">
-              Lorem ipsum dolor sit amet consectetur. Lobortis porta volutpat
-              tellus pellentesque sodales eget quam enim. Risus et diam quis
-              risus nunc ut porttitor tellus imperdiet. Id nunc turpis donec
-              aliquam amet aliquam quam.
+          <div className="m-7 mt-14 md:mt-13 sm:mt-20 md:w-full">
+            <p className="text-white md:w-[70vw] md:m-auto md:text-center">
+            Lorem ipsum dolor sit amet consectetur. Lobortis porta volutpat tellus pellentesque sodales eget quam enim. Risus et diam quis risus nunc ut porttitor tellus imperdiet. Id nunc turpis donec aliquam amet aliquam quam. Risus pellentesque aliquet nunc eu.
             </p>
           </div>
         </div>
