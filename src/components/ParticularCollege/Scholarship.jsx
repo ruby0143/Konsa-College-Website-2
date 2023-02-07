75c9270a782b007dd3bd93368a0433a40dbd54ad
Dev Kumar <nubdevs>
2023-02-07 11:56:38 +0530

    icon size fixed

--- src/components/ParticularCollege/Scholarship.jsx
+++ src/components/ParticularCollege/Scholarship.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import "./Scholarship.css";
 
 function Scholarship(props) {
   return (
     <div className="container">
       <div className="cnt-head text-xl m-3 font-bold text-[#303030] mt-8">Scholarship</div>
       <hr />
-      <div className="cnt-body fs mt-[1.5rem]">
+      <div className="cnt-body text-[13px] mt-[1.5rem]">
         {props.result.scholarships?.map((point, id) => {
           return (
-            <div className="s-item" key={id}>
-              <div className="s-icon">
-                <img className="icon h-3" src="https://konsa-college-website-icons.s3.ap-northeast-1.amazonaws.com/assets/icons/scholar.png" />
+            <div className="m-2 w-full flex" key={id}>
+              <div className="pt-[5px] mr-[8px] w-[5%]">
+                <img className="w-[30px] h-3" src="https://konsa-college-website-icons.s3.ap-northeast-1.amazonaws.com/assets/icons/scholar.png" />
               </div>
-              <p className="s-text">
+              <div className="w-[95%]"><p className="leading-[15.23px] tracking-[.05rem]">
                 {point}
-              </p>
+              </p></div>
             </div>
           )
         })}
