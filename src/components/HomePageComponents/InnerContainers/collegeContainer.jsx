ef28b60d3b0c82e1964d51e2f006772ff3a105cb
Dev Kumar <nubdevs>
2023-02-09 08:51:44 +0530

    all colleges mobile view card size fixed

--- src/components/HomePageComponents/InnerContainers/collegeContainer.jsx
+++ src/components/HomePageComponents/InnerContainers/collegeContainer.jsx
@@ -10,20 +10,20 @@ const CollegeContainer = ({
 }) => {
   return (
     <Link to={"/college/" + collegeId}>
-      <div className="min-w-[230px] max-w-[305px]  h-[164px] md:h-[188px] xs:[250px] shadow-md rounded-md border border-gray-100 bg-white">
+      <div className="min-w-[230px] max-w-[305px]  h-[167px] md:h-[188px] xs:[250px] shadow-md rounded-md border border-gray-100 bg-white">
         <div className="relative h-[110px] md:h-[120px] rounded-t-md flex items-end justify-center">
           <div className={`absolute brightness-50 w-full`}>
             <img
               src={collegeBanner}
               alt="College Banner"
-              className="h-[110px] md:h-[130px] w-full"
+              className="h-[128px] md:h-[130px] w-full"
             />
           </div>
           <div className="bg-white w-[50px] h-[50px] rounded-full z-10 mb-2">
             <img src={collegeLogo} alt="College Logo" />
           </div>
         </div>
-        <div className="h-[54px] md:h-[60px] flex items-center justify-center">
+        <div className="h-[52px] md:h-[60px] flex items-center justify-center">
           <div className="text-sm md:text-base text-center w-[90%] custom-truncate">
             {collegeName}
           </div>
