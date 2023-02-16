956188c7ff1c017b944dcab7bfbf40c6804709ee
ruby0143 <mysoresriharsha07@gmail.com>
2023-02-16 23:09:58 +0530

    trend analysis tool ui made dynamic

--- src/pages/trendAnalysis.jsx
+++ src/pages/trendAnalysis.jsx
@@ -4,12 +4,74 @@ import axios from "axios";
 
 function trendAnalysis() {
   const [colleges, setColleges] = useState([]);
+  const [branches, setBranches] = useState(new Map());
+  const [filteredBranches, setFilteredBranches] = useState([]);
+  const [selectedRank, setRank] = useState("Mains");
+  const [selectedState, setState] = useState(null);
   const [selectedCollege, setSelectefCollege] = useState(null);
-  
+  const [selectedBranch, setBranch] = useState(null);
+  const [selectedCaste, setCaste] = useState(null);
+  const [selectedGender, setGender] = useState("Gender-Neutral");
+  const [selectedInstTypes, setTypes] = useState([]);
+  const [requiredState, setRequiredState] = useState(false);
+  const [requiredInst, setReqInst] = useState(false);
+  const [requiredProg, setReqProg] = useState(false);
+  const [reqSeat, setReqseat] = useState(false);
+
+  const castes = [
+    "EWS",
+    "EWS (PwD)",
+    "OBC-NCL",
+    "OBC-NCL (PwD)",
+    "OPEN",
+    "OPEN (PwD)",
+    "SC",
+    "SC (PwD)",
+    "ST",
+    "ST (PwD)",
+  ];
+
+  const states = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu and Kashmir",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttarakhand",
+    "Uttar Pradesh",
+    "West Bengal",
+    "Andaman and Nicobar Islands",
+    "Chandigarh",
+    "Dadra and Nagar Haveli",
+    "Daman and Diu",
+    "Delhi",
+    "Lakshadweep",
+    "Puducherry",
+  ];
 
   useEffect(() => {
     const ele = document.querySelector("#chart");
-    console.log(ele, "ok");
 
     const chart = createChart(ele, { width: 500, height: 300 });
     const lineSeries = chart.addLineSeries();
@@ -27,15 +89,40 @@ function trendAnalysis() {
     ]);
 
     axios
-      .get(
-        "https://konsa-college-backend-production-0c4c.up.railway.app/allcolleges"
-      )
+      .get("https://konsa-college-backend-production-0c4c.up.railway.app/branches")
       .then((res) => {
-        setColleges(res.data);
+        const arr = res.data;
+        arr.forEach((ele) => {
+          const col = ele.Institute;
+          setColleges(function (prevState) {
+            return [...prevState, col];
+          });
+          const programs = ele.Array;
+          const arrPgs = programs.split("'");
+          setFilteredBranches(arrPgs);
+          branches.set(col, arrPgs);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }, []);
-  console.log(colleges, "ok");
-  console.log(selectedCollege, "okk");
+
+  // console.log(colleges,"ok");
+  // console.log(selectedCollege, "okk");
+  // console.log(filteredBranches, "br123");
+  // console.log(branches.get("Nehru National Institute of Technology, Durgapur"),'br');
+  console.log(
+    selectedRank,
+    selectedState,
+    selectedInstTypes,
+    selectedCollege,
+    selectedBranch,
+    selectedCaste,
+    selectedGender,
+    "details"
+  );
+
   return (
     <div className="p-3 ">
       <div className="head md:p-7">
@@ -55,11 +142,22 @@ function trendAnalysis() {
             <div>Rank Type</div>
             <div className="py-2 flex justify-between">
               <div>
-                <input type="radio" value="Mains"></input>
+                <input
+                  type="radio"
+                  value="Mains"
+                  onChange={(e) => setRank(e.target.value)}
+                  checked="checked"
+                  name="rank"
+                ></input>
                 <label>JEE (Main)</label>
               </div>
               <div>
-                <input type="radio" value="Advance"></input>
+                <input
+                  type="radio"
+                  value="Advance"
+                  name="rank"
+                  onChange={(e) => setRank(e.target.value)}
+                ></input>
                 <label>JEE (Advance)</label>
               </div>
             </div>
@@ -73,54 +171,101 @@ function trendAnalysis() {
             </div>
             <select
               name="states"
-              className="my-3 p-2 w-full border-solid border-[#D1D5DB] border rounded-md"
+              className="my-3 p-2 w-full border-solid border-[#D1D5DB] border rounded-md "
+              onChange={(e) => {
+                setState(e.target.value);
+                setRequiredState(false);
+              }}
             >
-              <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+              <option selected disabled>
+                Select a state
+              </option>
+              {states.map((state) => {
+                return <option value={state}>{state}</option>;
+              })}
             </select>
+            {requiredState ? (
+              <div className="py-2 text-xs text-red-500">
+                Home State is a required field
+              </div>
+            ) : null}
           </div>
           <div className="instituteTypes my-3 md:px-10 md:w-[33%]">
             <div className="flex justify-between">
-              <span>Home State</span>
+              <span>Institute Types</span>
             </div>
             <div className="opts mt-3 flex justify-between">
               <div className="">
-                <input type="checkbox"></input>
+                <input
+                  type="checkbox"
+                  value="NITs"
+                  onChange={(e) =>
+                    setTypes(function (prev) {
+                      return [...prev, e.target.value];
+                    })
+                  }
+                ></input>
                 <label className="px-2">NITs</label>
               </div>
               <div className="">
-                <input type="checkbox"></input>
+                <input
+                  type="checkbox"
+                  value="IITs"
+                  onChange={(e) =>
+                    setTypes(function (prev) {
+                      return [...prev, e.target.value];
+                    })
+                  }
+                ></input>
                 <label className="px-2">IITs</label>
               </div>
               <div className="">
-                <input type="checkbox"></input>
+                <input
+                  type="checkbox"
+                  value="GFTIs"
+                  onChange={(e) =>
+                    setTypes(function (prev) {
+                      return [...prev, e.target.value];
+                    })
+                  }
+                ></input>
                 <label className="px-2">GFTIs</label>
               </div>
             </div>
           </div>
         </div>
         <div className="flex flex-col md:flex-row md:justify-between">
-          <div className="institutes my-3 md:px-10 md:w-[33%]">
+          <div className="institutes my-3 md:px-10 md:w-[50%]">
             <div className="flex justify-between">
-              <span>Institue</span>
+              <span>Institute</span>
             </div>
             <select
               name="colleges"
               className="my-3 p-2 w-full border-solid border-[#D1D5DB] border rounded-md"
               onChange={(e) => {
                 setSelectefCollege(e.target.value);
+                setReqInst(false);
+                setFilteredBranches(branches.get(e.target.value));
+                if (selectedState === null) {
+                  setRequiredState(true);
+                }
               }}
             >
+              <option selected disabled>
+                Select a college
+              </option>
               {colleges?.map((college, idx) => {
-                return (
-                  <option value={college.college_name}>
-                    {college.college_name}
-                  </option>
-                );
+                return <option value={college}>{college}</option>;
               })}
             </select>
+            {requiredInst ? (
+              <div className="py-2 text-xs text-red-500">
+                College field is required
+              </div>
+            ) : null}
           </div>
 
-          <div className="course my-3 md:px-10 md:w-[33%]">
+          {/* <div className="course my-3 md:px-10 md:w-[33%]">
             <div className="flex justify-between">
               <span>Course</span>
             </div>
@@ -130,18 +275,34 @@ function trendAnalysis() {
             >
               <option value="Arunachal Pradesh">Arunachal Pradesh</option>
             </select>
-          </div>
+          </div> */}
           {selectedCollege ? (
-            <div className="program my-3 md:px-10 md:w-[33%]">
+            <div className="program my-3 md:px-10 md:w-[50%]">
               <div className="flex justify-between">
                 <span>Program</span>
               </div>
               <select
-                name="states"
+                name="programs"
                 className="my-3 p-2 w-full border-solid border-[#D1D5DB] border rounded-md"
+                onChange={(e) => {
+                  setBranch(e.target.value);
+                  setReqProg(false);
+                }}
               >
-                <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+                <option selected disabled>
+                  Select a program
+                </option>
+                {filteredBranches?.map((branch, idx) => {
+                  if (idx % 2 != 0) {
+                    return <option value={branch}>{branch}</option>;
+                  }
+                })}
               </select>
+              {requiredProg ? (
+                <div className="py-2 text-xs text-red-500">
+                  Program field is required
+                </div>
+              ) : null}
             </div>
           ) : null}
         </div>
@@ -151,21 +312,56 @@ function trendAnalysis() {
               <span>Seat Type</span>
             </div>
             <select
-              name="states"
+              name="castes"
               className="my-3 p-2 w-full border-solid border-[#D1D5DB] border rounded-md"
+              onChange={(e) => {
+                setCaste(e.target.value);
+                if (selectedCollege === null) {
+                  setReqInst(true);
+                }
+                if (selectedBranch === null) {
+                  setReqProg(true);
+                }
+                setReqseat(false);
+              }}
             >
-              <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+              <option selected disabled>
+                Select a caste
+              </option>
+              {castes.map((caste) => {
+                return <option value={caste}>{caste}</option>;
+              })}
             </select>
+            {reqSeat ? (
+              <div className="py-2 text-xs text-red-500">
+                Program field is required
+              </div>
+            ) : null}
           </div>
           <div className="gender my-3 md:w-[50%] md:px-10">
             <div className="flex justify-between">
               <span>Gender</span>
             </div>
             <select
-              name="states"
+              name="gender"
               className="my-3 p-2 w-full border-solid border-[#D1D5DB] border rounded-md"
+              onChange={(e) => {
+                setGender(e.target.value);
+                if (selectedCaste === null) {
+                  setReqseat(true);
+                }
+                if(selectedCollege === null){
+                  setReqInst(true);
+                }
+                if(selectedBranch === null){
+                  setReqProg(true);
+                }
+              }}
             >
-              <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+              <option value="Female-Only">Female Only</option>
+              <option selected value="Gender-Neutral">
+                Gender Neutral
+              </option>
             </select>
           </div>
         </div>
