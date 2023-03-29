43287f4e18a5f91be30194b4014b24560381e3f5
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-03-30 04:33:58 +0530

    [authModal.jsx] height error fixed

--- src/components/UI Components/Modal/authModal.jsx
+++ src/components/UI Components/Modal/authModal.jsx
@@ -6,7 +6,7 @@ const AuthModal = ({children}) => {
         className='fixed top-0 left-0 w-full h-full bg-[#00000080] z-[999]'
     >
       <div 
-        className='fixed md:rounded-sm top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#EE7C00] z-[1000] md:w-[75rem] mob:w-[100%] md:h-[45rem] mob:h-[100vh]'
+        className='fixed md:rounded-sm top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#EE7C00] z-[1000] md:w-[75rem] mob:w-[100%] md:h-[45rem] mob:h-full'
       >
         {children}
       </div>
