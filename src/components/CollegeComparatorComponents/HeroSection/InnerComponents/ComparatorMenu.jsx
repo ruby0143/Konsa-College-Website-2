7c1a896778386a140b654da1a29c4182a4f2ce6b
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-04-05 03:37:51 +0530

    [ComparatorMenu.jsx] college container responsiveness done on the basis of innerWidth

--- src/components/CollegeComparatorComponents/HeroSection/InnerComponents/ComparatorMenu.jsx
+++ src/components/CollegeComparatorComponents/HeroSection/InnerComponents/ComparatorMenu.jsx
@@ -4,28 +4,26 @@ import CollegeSelectContainer from './CollegeSelectContainer'
 
 const ComparatorMenu = () => {
 
-  const [collegeContainerCount, setCollgeContainerCount] = useState(4)
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
   useEffect(()=>{
     const handleResize = () =>{
-      if(window.innerWidth <= 600) setCollgeContainerCount(2);
-      else if(window.innerWidth > 600 && window.innerWidth <= 1200) setCollgeContainerCount(3)
-      else setCollgeContainerCount(4)
+      setWindowWidth(window.innerWidth)
     }
     window.addEventListener("resize",handleResize)
     return () => {
       window.removeEventListener("resize",handleResize)
     }
   },[])
-
+  
   return (
     <div className='z-10 bg-white rounded-sm shadow-md absolute w-[90%] mx-auto'>  
       <div className='flex w-full justify-between items-center'>
-        {[...Array(collegeContainerCount).fill(null)].map((_,idx) => {
+        {[...Array(windowWidth <= 600 ? 2 : 4).fill(null)].map((_,idx) => {
           return <CollegeSelectContainer key={idx}/>
         })}
       </div>
-      <div className='bg-white px-[24px] mob:px-[8px] py-[24px] mob:py-[8px] w-full flex items-center justify-end'>
+      <div className='bg-white px-[24px] mob:px-[8px] py-[24px] mob:py-[8px] w-full flex items-center justify-end mob:justify-center'>
         {<button className='py-2 mob:py-1 w-[356px] mob:w-full text-white font-medium bg-[#EE7C00] rounded-sm cursor-pointer'>
           Compare
         </button>}
